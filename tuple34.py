9f5b12f18e8957e8390d27465d309aef2c914d3d
shivaneepattanaik11 <pattanaikshivanee695@gmail.com>
2026-01-24 13:58:52 +0530

    assignment3

---
+++ tuple34.py
@@ -0,0 +1,10 @@
+#assignment3
+tup1=("shivanee","pattanaik",34)
+tup2=(1,2,3,4)
+print(tup1)
+print(tup2)
+tuple=tup1+tup2
+print("concatenated tuple:",tuple)
+n_tuple=((tup1),(tup2))
+print("nested tuple:",n_tuple)
+print("indexing in nested tuple:",n_tuple[1][2])
