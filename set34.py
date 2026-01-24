36bdae00b95e141b92cb65a51f3be94e8cd51c5d
shivaneepattanaik11 <pattanaikshivanee695@gmail.com>
2026-01-24 13:57:54 +0530

    Delete set34.py

--- set34.py
+++
@@ -1,9 +0,0 @@
-#assignment2
-set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-for i in set:
-    print(i) #to access elements.
-set1 ={1, 2, 13, 4, 15}
-print("union:",set|set1)
-print("intersection:",set&set1)
-print("difference:",set-set1)
-print("intersection:",set.intersection(set1))
